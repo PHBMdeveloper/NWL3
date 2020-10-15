ee729eb3eef7a3c64ed023ee16643fc34912806a
PHDeveloper <phbm@hotmail.com>
2020-10-15 02:58:55 -0300

    Adicionando o mapa e estilos

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import { FiPlus } from 'react-icons/fi'
-import { Map } from 'react-leaflet'
-import mapMarkerImg from '../images/map-marker.svg'
-import '../styles/pages/orphanages-map.css'
+import React from 'react';
+import {Link} from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import mapMarkerImg from '../images/map-marker.svg';
+import '../styles/pages/orphanages-map.css';
 
 function OrphanegesMap() {
   return (
@@ -19,7 +20,19 @@ function OrphanegesMap() {
           <span>Brasil</span>
         </footer>
       </aside>
-      <div></div>
+
+      <Map
+        center={[-23.5649068,-46.6370649]}
+        zoom={12}
+        style={{width: '100%', height: '100%'}}
+      >
+        {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+        />
+      </Map>
+      
+
       <Link to="" className="create-orphanage">
         <FiPlus size={32} color="#FFF"/>
       </Link>
