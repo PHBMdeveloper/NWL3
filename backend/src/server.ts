323fb5141aec4c3c1d4105fa245da14ae4433022
PHDeveloper <phbm@hotmail.com>
2020-10-20 01:54:09 -0300

    Pegando valor na rota

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,6 +6,7 @@ app.use(express.json())
 
 app.post('/users/:id', (request, response) =>{
     console.log('query', request.query)
+    console.log('params', request.params)
     return response.json({message: 'OK'})
 })
 
