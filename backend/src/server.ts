73d366ed675872bad4d9e3c012aa82e5a1e00ea9
PHDeveloper <phbm@hotmail.com>
2020-10-20 01:56:36 -0300

    Iniciando depois dos exemplos

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,11 +4,8 @@ const app = express()
 app.use(express.json())
 
 
-app.post('/users/:id', (request, response) =>{
-    console.log('query', request.query) //
-    console.log('params', request.params) //
-    console.log('body', request.body) //
-    return response.json({message: 'OK'}) //
+app.post('/users', (request, response) =>{
+    return response.json({message: 'OK'}) 
 })
 
 app.listen(3333)
