8cc1c2fb006961d37462924d64bef262c05c5be2
PHDeveloper <phbm@hotmail.com>
2020-10-20 01:54:25 -0300

    Pegando valor pelo corpo da requisição

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,6 +7,7 @@ app.use(express.json())
 app.post('/users/:id', (request, response) =>{
     console.log('query', request.query)
     console.log('params', request.params)
+    console.log('body', request.body)
     return response.json({message: 'OK'})
 })
 
