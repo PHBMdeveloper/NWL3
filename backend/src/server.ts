1517af7b918ba067321dd29caa7573859e28c1fe
PHDeveloper <phbm@hotmail.com>
2020-10-21 15:30:44 -0300

    Gravação no banco via insominia ok

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,40 @@
 import express from 'express'
+import { getRepository } from 'typeorm'
+import Orphanage from './models/Orphanage'
 import './database/connection'
+
 
 const app = express()
 app.use(express.json())
 
 
-app.post('/users', (request, response) =>{
-    return response.json({message: 'OK'}) 
+app.post('/orphanages', async (request, response) => {
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends
+  } = request.body
+  // console.log(request.body)
+
+  const orphanagesRepository = getRepository(Orphanage)
+
+  const orphanage = orphanagesRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends
+  })
+
+  await orphanagesRepository.save(orphanage)
+
+  return response.json({ message: 'OK' })
 })
 
 app.listen(3333)
