6cb4a23fc1216862374128680de974e7d7ac765e
PHDeveloper <phbm@hotmail.com>
2020-10-21 14:11:00 -0300

    "typeorm migration:create -n create_orphanages"

---
+++ backend/src/database/migrations/1603299639906-create_orphanages.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class createOrphanages1603299639906 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
