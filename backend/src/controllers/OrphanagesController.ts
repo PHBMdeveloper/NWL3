38af3424a5d30e5edf802ad3df8d1b2ce0a6ece1
PHDeveloper <phbm@hotmail.com>
2020-10-21 18:40:16 -0300

    Validação de dados OK com Yuo

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { getRepository } from 'typeorm'
 import orphanageView from "../views/orphanages_views";
+import * as Yup from 'yup'
 import Orphanage from '../models/Orphanage'
 
 export default {
@@ -49,8 +50,8 @@ export default {
       return { path: image.filename }
     })
 
-    const orphanage = orphanagesRepository.create({
-      nome,
+    const data = {
+      name,
       latitude,
       longitude,
       about,
@@ -58,7 +59,28 @@ export default {
       opening_hours,
       open_on_weekends,
       images
+    }
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      latitude: Yup.number().required(),
+      longitude: Yup.number().required(),
+      about: Yup.string().required().max(300),
+      instructions: Yup.string().required().max(300),
+      opening_hours: Yup.string().required().max(300),
+      open_on_weekends: Yup.boolean().required(),
+      images: Yup.array(
+        Yup.object().shape({
+          path: Yup.string().required()
+        })
+      )
     })
+
+    await schema.validate(data, {
+      abortEarly: false,
+    })
+
+    const orphanage = orphanagesRepository.create(data)
 
     await orphanagesRepository.save(orphanage)
 
